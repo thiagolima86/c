5d4894dd2dbf76ccff81decf3212f66a612899ff
Thiago Lima <thiagolima86@gmail.com>
2020-03-15 21:49:06 -0300

    area circle

---
+++ area_of_circle.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#define PI 3.14159
+
+int main()
+{
+    float ray, circle_area;
+    printf("what the ray of circle?");
+    scanf("%f", &ray);
+    circle_area = PI * pow(ray,2);
+    printf("%f", circle_area);
+    return 0;
+}

f6040debffd229923d1f9b9bb0cdf787b5b25658
Renan Oliveira <renan.oliveira.webdesigner@gmail.com>
2020-03-17 17:44:12 -0300

    Respostas para valores em que B ou C é zero

--- baskara.c
+++ baskara.c
@@ -16,6 +16,18 @@ int main() {
         printf("Impossivel calcular");
         return 0;
     }
+
+    if (b == 0) {
+        x1 = - (c / a);
+        printf("√%f", x1);
+        return 0;
+    }
+
+    if (c == 0) {
+        x1 = - (b / a);
+        printf("%f", x1);
+        return 0;
+    }
     
     delta = (b * b) - 4 * a * c;
 
